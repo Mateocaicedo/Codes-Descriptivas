d5deb7eb6490f39c37d0bebc668a7b43a53d84d0
Mateocaicedo <caicedomateo9@gmail.com>
2022-04-10 19:54:43 -0500

    modificado

--- Taller.R
+++ Taller.R
@@ -2,18 +2,19 @@ library(prob)
 library(combinat)
 Omega <-rolldie(2)#dados
 Omega
-Espacio_muestral <-dim(Omega); Espacio_muestral
+muestra<-dim(Omega); muestra
 
-EventoB <- subset(Omega,(X1+X2) ==7); EventoB
+B <- subset(Omega,(X1+X2) ==7); B
 
-EventoC <- subset(Omega,(X1+X2) >=11); EventoC
+C <- subset(Omega,(X1+X2) >=11);C
 
-EventoD <- subset(Omega,(X1+X2) <=2); EventoD
+D <- subset(Omega,(X1+X2) <=2); D
 
-EventoE <- subset(Omega,(X1/X2) ==1); EventoE
+E <- subset(Omega,(X1/X2) ==1); E
 
-EventoF <- subset(Omega,(X1/X2) !=1); EventoF
+F <- subset(Omega,(X1/X2) !=1 ); F
 
-unionB_F<-union(EventoB, EventoF); unionB_F
-interseccionC_E <-intersect(EventoC, EventoE); interseccionC_E
-diferenciaC_F<- setdiff(EventoC, EventoF); diferenciaC_F
+unionB_F<-union(B, F); unionB_F
+interseccionC_E <-intersect(C, E); interseccionC_E
+diferenciaC_F<- setdiff(C, F); diferenciaC_F
+  
